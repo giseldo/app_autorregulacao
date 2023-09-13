d422de44d0645059a724f9adc1eebd6e12284a86
Giseldo Neo <giseldo@gmail.com>
2023-09-13 11:35:22 -0300

    ultima versao

---
+++ pages/10_Login.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import streamlit_google_oauth as oauth
+
+login_info = oauth.login(client_id="80547735486-qlqurma8i51tkdr8aqp5lncb25in1snd.apps.googleusercontent.com",
+        client_secret="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",
+        redirect_uri="http://localhost:8501/",
+        logout_button_text="Logout"
+    )
+
+if login_info:
+        user_id, user_email = login_info
+        st.write(f"Welcome {user_email}")
+else:
+        st.write("Please login")
